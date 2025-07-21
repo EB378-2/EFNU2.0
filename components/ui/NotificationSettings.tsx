b7c493e2c4080218f4ab2639dd9c447a592f0603
EB378-2 <eric.benni@syk.fi>
2025-07-21 16:09:04 +0000

    modified:   components/ui/NotificationSettings.tsx

--- components/ui/NotificationSettings.tsx
+++ components/ui/NotificationSettings.tsx
@@ -1,9 +1,11 @@
 // components/ui/NotificationSettings.tsx
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Notifications, NotificationsOff } from '@mui/icons-material';
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Tooltip } from '@mui/material';
+import OneSignal from 'react-onesignal';
+
 
 
 export function NotificationSettings() {
@@ -38,10 +40,26 @@ export function NotificationSettings() {
     }
   };
 
+    window.OneSignalDeferred = window.OneSignalDeferred || [];
+    useEffect(() => {
+    OneSignal.init({
+      appId: "68782627-f22d-45b7-b1eb-44b826777b50"
+    });
+  }, []);
+
+ 
+  const onHandleTag = (tag: string) => {
+    OneSignal.User.addTag("user_role", tag)
+    console.log('Tagging');
+    
+  }
+
+
+
     return (
     <Box display="flex" alignItems="center" gap={2}>
           <IconButton
-            onClick={toggleSubscription}
+            onClick={() => onHandleTag('admin')}
             disabled={permission === 'denied' || enabled === null}
             color={enabled ? 'primary' : 'default'}
           >
