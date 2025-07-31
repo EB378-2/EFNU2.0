a52e323da05e12f0bf6e6c285baa1544f5103591
EB378-2 <eric.benni@syk.fi>
2025-07-31 19:12:47 +0000

    s
        modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,6 @@ const baseConfig = {
       headers: [
         { key: 'X-Content-Type-Options', value: 'nosniff' },
         { key: 'X-Frame-Options', value: 'DENY' },
-        { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
       ],
     },
     {
